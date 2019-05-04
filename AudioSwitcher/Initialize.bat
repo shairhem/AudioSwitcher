5a778e8ee0725d83b6e499a89a57647d46ca1ccf
Shairhem Fernandez <shairhem@gmail.com>
2019-05-04 13:15:30 +0800

    Add script to run AudioSwitcher as background task

---
+++ AudioSwitcher/Initialize.bat
@@ -0,0 +1,1 @@
+﻿sc create AudioSwitcher displayname="Audio Switcher" binpath="D:\Scripts\AudioSwitcher" start=auto && sc description AudioSwitcher "Backgroud service to have a audio switcher in tray"
